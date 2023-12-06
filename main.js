dbf8c7076a8fb6db5ac656af6a1906bbd5d993e5
yanquielarango <yanquielwebdesigner@gmail.com>
2023-12-06 13:02:56 +0100

    refactor: create a navbar functionality in a JavaScript file

---
+++ main.js
@@ -0,0 +1,17 @@
+/* SHOW MENU*/
+
+const navMenu = document.querySelector('#nav-menu');
+const navToggle = document.querySelector('#nav-toggle');
+const navClose = document.querySelector('#nav-close');
+
+/* MENU SHOW */ 
+navToggle.addEventListener('click', () => {
+  navMenu.classList.add('show-menu');
+});
+
+
+
+/*/ MENU HIDDEN */ 
+navClose.addEventListener('click', () => {
+  navMenu.classList.remove('show-menu');
+});
