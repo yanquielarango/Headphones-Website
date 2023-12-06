bba3f86a7a45710e62322aea6d6c2fb576156c17
yanquielarango <yanquielwebdesigner@gmail.com>
2023-12-06 14:03:40 +0100

    modify JavasScipt file

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 const navMenu = document.querySelector('#nav-menu');
 const navToggle = document.querySelector('#nav-toggle');
 const navClose = document.querySelector('#nav-close');
-
+const navLink = document.querySelectorAll('.nav__link');
 /* MENU SHOW */ 
 navToggle.addEventListener('click', () => {
   navMenu.classList.add('show-menu');
@@ -15,3 +15,12 @@ navToggle.addEventListener('click', () => {
 navClose.addEventListener('click', () => {
   navMenu.classList.remove('show-menu');
 });
+
+
+/* REMOVE FROM MOBILE */
+const  linkAction = () => {
+  const navMenu = document.querySelector('#nav-menu');
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
